18c2b4faa2a73b16013c9d8bde609c8b677b2428
Pankaj Tandon <pankajtandon@gmail.com>
2025-11-17 18:56:45 -0500

    Added proper response annotations for LLM consumption

--- src/main/java/com/technochord/mcp/airbnb/domain/Property.java
+++ src/main/java/com/technochord/mcp/airbnb/domain/Property.java
@@ -1,16 +1,14 @@
 package com.technochord.mcp.airbnb.domain;
 
-import org.springframework.ai.tool.annotation.ToolParam;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 
 import java.util.List;
 
-//TODO: ToolParams dont really have a place here because they are not input.
-//How does the LLM interpret the response?
-public record Property(@ToolParam(required = true, description = "The Id of the property") String id,
-                       @ToolParam(required = true, description = "The address of the property") String address,
-                       @ToolParam(required = true, description = "The number of bedrooms in the property") int numberBedrooms,
-                       @ToolParam(required = true, description = "The number of bathrooms in the property") int numberBathrooms,
-                       @ToolParam(required = true, description = "The number of adults and children that it can house") int capacity,
-                       @ToolParam(required = false, description = "The ameneties in the room or property") List<String> amenetiesList,
-                       @ToolParam(required = true, description = "The rent per day of stay in USD") double rentPerNight) {
+public record Property(@JsonPropertyDescription("The Id of the property") String id,
+                       @JsonPropertyDescription("The address of the property") String address,
+                       @JsonPropertyDescription("The number of bedrooms in the property") int numberBedrooms,
+                       @JsonPropertyDescription("The number of bathrooms in the property") int numberBathrooms,
+                       @JsonPropertyDescription("The number of adults and children that it can house") int capacity,
+                       @JsonPropertyDescription("The ameneties in the room or property") List<String> amenetiesList,
+                       @JsonPropertyDescription("The rent per day of stay in USD") double rentPerNight) {
 }
